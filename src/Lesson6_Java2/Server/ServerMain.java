ba266cac1b4a43b99d2434186bb68bcc6435a174
Иван <ivan_chcepelkin@mail.ru>
2018-07-19 16:59:46 +0300

    Main_DZ6 "Клиент - сервер" Запилил клиента и сервер

--- src/Lesson6_Java2/Server/ServerMain.java
+++ src/Lesson6_Java2/Server/ServerMain.java
@@ -1,12 +1,14 @@
-package Lesson6_Java2;
+package Lesson6_Java2.Server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ServerMain {
+public class  ServerMain {
     public static void main(String[] args) {
 
 
@@ -18,11 +20,18 @@ public class ServerMain {
             socket = server.accept();//ждём подключения клиента
             System.out.println("Клиент подключился!");
 
-            Scanner in = new Scanner(socket.getInputStream());//справшиваем входящий поток
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);//исходяший поток
+//            Scanner in = new Scanner(socket.getInputStream());//справшиваем входящий поток
+//            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);//исходяший поток
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             while (true) {
-                String str = in.nextLine();//записываем в сканер
-                System.out.println("Клиент " + str);//выводим данные от клиента
+                String str = in.readUTF();//записываем
+//                System.out.println("Клиент " + str);//выводим данные от клиента
+                if (str.equals("/end")) {
+                    out.writeUTF("/serverClosed");
+                    break;
+                }
+                out.writeUTF(str);//отправляем сообщение обратно
             }
         } catch (IOException e) {
             e.printStackTrace();
---
+++ src/Lesson6_Java2/client/Controller.java
@@ -0,0 +1,81 @@
+package Lesson6_Java2.client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public  class Controller implements Initializable {
+    @FXML
+    TextArea textArea;//указываем id из sample
+    @FXML
+    TextField textField;
+
+    Socket socket;
+
+    DataInputStream in;
+    DataOutputStream out;
+    final String IP = "localhost"; //create ip adress
+    final int Port = 8189;//create port
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            socket = new Socket(IP,Port);//create new socket
+            in = new DataInputStream(socket.getInputStream());//инициализация входящего потока
+            out = new DataOutputStream(socket.getOutputStream());
+
+            Thread t1 = new Thread(new Runnable() { //создаем поток входных данных
+                @Override
+                public void run() {
+
+                    try {
+                        while (true) { //сокет закроется, когда выйдем из цикла
+                            String str = in.readUTF();
+                            if (str.equals("/serverClosed")) {
+                                break;
+                            }
+                            textArea.appendText(str + "\n"); //выводим строку, что пришла от сервера
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            t1.setDaemon(true);//t1 как фоновый поток
+            t1.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+   public void sendMsg(){
+//       textArea.appendText(textField.getText()+"\n"); //передаем текст  из окна ввода сообщения в обший чат
+//       textField.clear();//очищаем окно ввода сообщений
+//       textField.requestFocus();//текстовый фокус остается на окне ввода сообщений
+       try {
+           out.writeUTF(textField.getText());//отправляем на сервер наше сообщение
+           textField.clear(); //очищаем textField
+           textField.requestFocus();//устанавливаем фокус
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+   }
+
+
+}
